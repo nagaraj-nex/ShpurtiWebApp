106f6c44c56fcd6bc2bd26e5ea023c9596e2a610
Nagaraj Ravinuthala <nagarajr@nexaria.com>
2021-06-13 20:36:14 +0530

    added js and css for filtering functionality

--- ShpurtiWebApp/settings.py
+++ ShpurtiWebApp/settings.py
@@ -18,7 +18,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print("BASE_DIR :: ", BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
